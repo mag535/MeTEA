7f01de5ff4a845d308ffc8a32c1152775dd0e162
NuMosaic <mag535@drexel.edu>
2020-10-07 10:31:05 -0400

    new package name for installing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-setup(name="TEA",
+setup(name="meTEA",
         version="0.0.1",
         author="Melissa Gray",
         author_email="mag535@drexel.edu",
