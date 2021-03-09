3d143eef2b342b1356f948db2ee93f050ea75cef
local_mag <mag535@drexel.edu>
2021-03-09 15:39:02 -0500

    new files for package update

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-requirements = ["numpy>=1", "pandas>=1", "openpyxl>=3"]
-
-setup(name="MeTEA",
+setup(name="MeTEA-NuMosaic",
         version="0.0.3",
         author="Melissa Gray",
         author_email="mag535@drexel.edu",
@@ -16,11 +14,11 @@ setup(name="MeTEA",
         long_description_content_type="text/markdown",
         url="https://github.com/mag535/MeTEA/",
         packages=find_packages(),
-		install_requirements=requirements,
         classifiers=[
             "Operating System :: OS Independent",
             "Intended Audience :: Science/Research",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.7",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"]
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
+		python_requires=">=3.7"
         )
